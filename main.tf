f8cf2568adc150d78f7d673f0bee2b9cadd8ddd0
Phanee <phanee47@gmail.com>
2023-02-12 12:09:33 +0530

    added server side encryption code

--- main.tf
+++ main.tf
@@ -12,3 +12,19 @@ resource "aws_s3_bucket" "b" {
   }
 
 }
+
+resource "aws_kms_key" "mykey" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  bucket = aws_s3_bucket.b.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.mykey.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
