3ddd3565bd7d100a167c5b62644bf8692c0c511e
phanee <phanee47@gmail.com>
2023-04-05 10:03:25 +0530

    added lambda code

--- main.tf
+++ main.tf
@@ -1,6 +1,35 @@
-provider "aws" {
-    region = "us-east-1"
+data "aws_iam_policy_document" "lambda_assume_role_policy" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
 }
+
+resource "aws_iam_role" "lambda_role" {
+  name               = "lambda-lambdaRole-waf"
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role_policy.json
+}
+
+data "archive_file" "python_lambda_package" {
+  type        = "zip"
+  source_file = "${path.module}/code/lambda_function.py"
+  output_path = "nametest.zip"
+}
+
+resource "aws_lambda_function" "test_lambda_function" {
+  function_name    = "lambdaTest"
+  filename         = "nametest.zip"
+  source_code_hash = data.archive_file.python_lambda_package.output_base64sha256
+  role             = aws_iam_role.lambda_role.arn
+  runtime          = "python3.9"
+  handler          = "lambda_function.lambda_handler"
+  timeout          = 10
+}
+
 
 
 resource "aws_s3_bucket" "b" {
