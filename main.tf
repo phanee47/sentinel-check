6056895e1dae51e31af9b9a1da46449c0943ff88
phanee47 <67285782+phanee47@users.noreply.github.com>
2023-04-12 14:53:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -33,6 +33,10 @@ resource "aws_lambda_function" "test_lambda_function" {
     "Artifactory" = "Artifactory"
  
   }
+  vpc_config {
+    # Every subnet should be able to reach an EFS mount target in the same Availability Zone. Cross-AZ mounts are not permitted.
+    subnet_ids         = ["subnet-0a744e1e7ae41d699"]
+  }
 }
 
 
